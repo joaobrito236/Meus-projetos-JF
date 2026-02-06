0405d32e6fab203416412163a903844675330e72
joaobrito236 <joao.brito9@estudante.ifto.edu.br>
2026-02-05 21:40:45 -0300

    atualização

---
+++ index.php
@@ -0,0 +1,38 @@
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha primeira página</title>
+    <link rel="stylesheet" href="assets/styles.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <!-- CABEÇALHO DA MINHA PÁGINA  -->
+        <header>
+            <!-- IMAGEM -->
+            <img src="assets/imagem_rato.jpg" alt="imagem_rato">
+
+            <!-- TÍTULO -->
+            <h1>Esse é meu primeiro projeto WEB</h1>
+            <p>Me chamo João Francisco aluno do 3° período</p>
+        </header>
+
+        <!-- MENU DE NAVEGAÇÃO -->
+        <nav>
+            <ul>
+                <li><a href="#">Tags HTML</a></li>
+                <li><a href="#">CSS</a></li>
+            </ul>
+
+        </nav>
+
+
+
+    </div>
+
+</body>
+
+</html>
